25ed789b377516caf06db6de7d71a35d5377e7e1
parksubeen77 <bfbeen7@gmail.com>
2024-11-20 01:25:50 +0900

    flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,6 @@
 # flask_app.py
 from flask import Flask
-from discord_bot import run_discord_bot
+from discord_bot import run_discord_bot  # 봇 코드 import
 from threading import Thread
 
 app = Flask(__name__)
@@ -9,15 +9,15 @@ app = Flask(__name__)
 def home():
     return "Discord 봇이 실행 중입니다!"
 
-# Flask와 Discord 봇을 각각 별도의 스레드로 실행
+# Flask 서버 실행 함수
 def run_flask():
     app.run(host="0.0.0.0", port=5000)
 
 if __name__ == "__main__":
     # 봇을 별도의 스레드로 실행
-    t_bot = Thread(target=run_discord_bot)
+    t_bot = Thread(target=run_discord_bot, daemon=True)
     t_bot.start()
 
     # Flask 서버를 별도의 스레드로 실행
-    t_flask = Thread(target=run_flask)
-    t_flask.start()
+    print("Flask 서버가 시작되었습니다.")
+    run_flask()
