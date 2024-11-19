049159cdf016bc0672f56d91c75689db78e23ff5
parksubeen77 <bfbeen7@gmail.com>
2024-11-20 01:54:53 +0900

    flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,29 +1,30 @@
 import os
-from flask import Flask
 import subprocess
 import threading
+from flask import Flask
 
 app = Flask(__name__)
 
+# Flask에서 기본 경로
 @app.route('/')
 def home():
     return "Discord 봇이 실행 중입니다!"
 
 # Flask 서버 실행 함수
 def run_flask():
-    port = os.environ.get("PORT", 5000)  # 환경 변수에서 포트 번호를 가져옴
+    port = os.environ.get("PORT", 5000)  # Render의 환경 변수로 포트 설정
     app.run(host="0.0.0.0", port=int(port))
 
 # Discord 봇을 실행하는 함수
 def run_discord_bot():
-    subprocess.Popen(["python", "bot.py"])
+    subprocess.Popen(["python", "bot.py"])  # Discord 봇을 별도 프로세스로 실행
 
 # Flask와 Discord 봇을 동시에 실행하는 함수
 def run():
-    # Flask 서버를 별도 프로세스로 실행
+    # Flask 서버를 별도의 스레드에서 실행
     threading.Thread(target=run_flask).start()
 
-    # Discord 봇을 별도 프로세스로 실행
+    # Discord 봇을 별도의 프로세스로 실행
     run_discord_bot()
 
 if __name__ == "__main__":
